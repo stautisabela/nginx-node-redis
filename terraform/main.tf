ba92e8efe67f267356e9a648f9a77ff7396a09e5
stautisabela <stautisabela@gmail.com>
2026-03-06 13:44:52 +0100

    add terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "nginx-web-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["amazon"]
+  
+      filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+      }
+
+      filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+      }
+    }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io docker-compose git
+              systemctl start docker
+              systemctl enable docker
+              git clone https://github.com/stautisabela/nginx-node-redis.git 
+              cd nginx-node-redis/
+              docker-compose up -d --build
+              EOF
+
+  tags = {
+    Name = "nginx-docker-instance"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.web.public_ip
+}
