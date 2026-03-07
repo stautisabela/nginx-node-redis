1fea8daa4de86c545d5791c32e97e29ccb98dd84
stautisabela <stautisabela@gmail.com>
2026-03-07 09:58:39 +0100

    create terraform workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "web" {
               apt-get install -y docker.io docker-compose git
               systemctl start docker
               systemctl enable docker
-              git clone https://github.com/stautisabela/nginx-node-redis.git 
+              git clone https://github.com/stautisabela/nginx-node-redis
               cd nginx-node-redis/
               docker-compose up -d --build
               EOF
